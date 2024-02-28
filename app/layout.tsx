53d3cd734b05b4a6891b223bb6f7d457a4a7be29
Rodrigo Medeiros <rodrigonavarro171@gmail.com>
2024-02-28 20:55:00 -0300

    alterando o title da pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Rodrigo Medeiros",
   description: "Portfolio de Rodrigo Medeiros",
 };
 
